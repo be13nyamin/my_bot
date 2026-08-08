c1dcb93c7938bd09b53941bbf00a912a1b5671d5
be13nyamin <yazdibenyamin0@gmail.com>
2026-08-08 18:29:44 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# تنظیمات اصلی ربات
+TOKEN = '8642906741:AAGc-cXICORZ59kmIfSt3gA42BCDyASpq1Q' # حتماً توکن رو اینجا بذار
+OWNER_ID = 8441091476  # آیدی عددی تلگرام خودت رو اینجا بذار
+
+# لینک‌های تبلیغاتی
+GROUP_LINK = 'https://t.me/+EJGHR500H4ZhYTk8' # لینک گپ خودت
+CHANNEL_LINK = 'https://t.me/+AsYybSaGoUowOWQ8' # لینک چنل خودت
+
+# تنظیمات پیش‌فرض
+DEFAULT_WELCOME = "سلام! به ربات خوش اومدی. 😊"
