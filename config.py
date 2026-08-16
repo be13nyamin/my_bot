148cda8b45ea1929f27c6128d90bebba1927dbf5
be13nyamin <yazdibenyamin0@gmail.com>
2026-08-16 22:41:21 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+TOKEN = "8622448090:AAEXY0fx0-_noi8LDYAHGyb0evOrV_SXC0g"
+AI_API_KEY = ""          # اگر خواستی هوش مصنوعی وصل کنی, کلیدت رو بذار
+
+# آیدی عددی ادمین اصلی (تو)
+ADMIN_IDS = [8441091476]
+
+URL_PATTERN = r'(https?://\S+|www\.\S+|t\.me/\S+)'
+
+# عدد اخطارها
+WARN_LIMIT_LINK = 3      # ضد لینک: بعد از ۳ اخطار بن
+WARN_LIMIT_PROFILE = 5   # چک پروفایل: بعد از ۵ اخطار بن
+
+# اسامی فایل‌های داده (برای سرگرمی و نگهداری)
+DB_FILE = "batman_db.json"
